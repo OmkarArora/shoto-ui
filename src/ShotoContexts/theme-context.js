bdfce90b037cdbd4cccf178a635ea94d25c921a6
Omkar Arora <aroraomkar@gmail.com>
2021-03-26 17:13:56 +0530

    refactor: css size units, removed commented code

--- src/ShotoContexts/theme-context.js
+++ src/ShotoContexts/theme-context.js
@@ -1,20 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
-// import { darkTheme, lightTheme } from "../theme";
 
 const ShotoThemeContext = createContext();
-
-// export const ShotoThemeProvider = ({children}) =>{
-// 	const [theme, setTheme] = useState(darkTheme)
-// 	const changeTheme = newTheme => {
-// 		if(newTheme === "light") setTheme(lightTheme);
-// 		return setTheme(darkTheme);
-// 	}
-// 	return (
-// 		<ShotoThemeContext.Provider value={{theme, changeTheme}}>
-// 			{children}
-// 		</ShotoThemeContext.Provider>
-// 	)
-// }
 
 export const ShotoThemeProvider = ({children}) =>{
 	const [theme, setTheme] = useState("dark")
